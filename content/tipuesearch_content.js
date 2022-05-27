5317d01a3d1a8c200d85c045470bd399b07898d1
40923123 <40923123@gm.nfu.edu.tw>
2022-05-27 18:56:56 +0800

    20220527

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n test \n 20220419 \n 20220420 \n 20220427 \n 20220504 \n 20220508 \n 20220510 \n 20220511 \n 20220518 \n 20220525', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n test \n 20220419 \n 20220420 \n 20220427 \n 20220504 \n 20220508 \n 20220510 \n 20220511 \n 20220518 \n 20220525 \n 20220527', 'tags': '', 'url': 'About.html'}]};
